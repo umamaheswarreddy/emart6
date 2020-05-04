48d59cfa10ac097a185a3fde8a06a4cb4498ccf5
umamaheswar reddy <35299489+umamaheswarreddy@users.noreply.github.com>
2020-05-04 09:53:58 +0530

    Zuul with zuul filter

---
+++ Zuul-Proxy-Server/src/main/java/com/cts/ZuulProxyServerApplication.java
@@ -0,0 +1,21 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulProxyServerApplication {
+
+	public static void main(String[] args) {
+		
+		
+		SpringApplication.run(ZuulProxyServerApplication.class, args);
+		
+		
+	}
+
+}
